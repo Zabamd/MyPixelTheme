790aa63e8aded43abec4c72bc99755e4a8ddee96
zabierowski <zabierowski@nomachine.com>
2022-12-13 10:28:42 +0100

    added header to index template

--- build/defaultheading.asset.php
+++ build/defaultheading.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => 'd6c5291eaaab3d154df3');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => '2e201d367ece3da4630c');
