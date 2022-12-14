2560ef6495dbc46d0f55204c8589c491e5195065
zabierowski <zabierowski@nomachine.com>
2022-12-14 15:45:03 +0100

    Fixed webpack settings

--- build/editor-style.css.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '70bfb1843908eb383d08');
--- build/style.css.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c5c126b93f2eea395e18');
